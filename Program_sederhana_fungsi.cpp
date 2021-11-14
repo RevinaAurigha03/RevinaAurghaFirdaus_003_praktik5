2b25194af4ae01696858653f11d638bd2c7b93ce
RevinaAurigha03 <90344666+RevinaAurigha03@users.noreply.github.com>
2021-11-14 18:05:17 +0700

    Add files via upload

---
+++ Program_sederhana_fungsi.cpp
@@ -0,0 +1,19 @@
+//* REVINA AURIGHA FIRDAUS (21091397003)
+
+#include <iostream>
+#include <stdio.h>
+#include <conio.h>
+using namespace std;
+
+void garis();		//deklarasi fungsi
+main(){
+	system("cls");
+	garis();		//memanggil fungsi
+	cout<<" NO   NIM    NAMA     NILAI"<<endl;
+	garis();
+	getch();
+}
+void garis(){		//definisi fungsi
+	cout<<"=============================="<<endl;
+}
+
